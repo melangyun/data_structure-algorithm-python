963480a994a2e8387b8862ea3474769a9513d858
melangyun <melangyun@gmail.com>
2020-12-16 23:42:01 +0900

    heap insert 구현

---
+++ data_structure/10_heap.py
@@ -0,0 +1,35 @@
+# 힙은 완전 이진트리이기 때문에, 일반적으로 힙 구현시 배열 자료구조를 활용함
+
+class Heap:
+    def __init__ (self, data):
+        self.heap_array = list()
+        self.heap_array.append(None)
+        self.heap_array.append(data)
+
+    def move_up(self, inserted_idx):
+        if inserted_idx <= 1:
+            return False
+        
+        parent_idx = inserted_idx // 2
+        if self.heap_array[inserted_idx] > self.heap_array[parent_idx]:
+            return True
+        else:
+            return False
+    
+    def insert(self, data):
+        if len(self.heap_array) == 0:
+            self.heap_array.append(None)
+            self.heap_array.append(data)
+            return True
+        
+        self.heap_array.append(data)
+
+        inserted_idx = len(self.heap_array) - 1
+
+        while self.move_up(inserted_idx):
+            parent_idx = inserted_idx // 2
+            # swap하는 자주쓰는 python코드!
+            self.heap_array[inserted_idx], self.heap_array[parent_idx] = self.heap_array[parent_idx], self.heap_array[inserted_idx]
+            inserted_idx = parent_idx
+
+        return True
